778f03a1ff0240518557c9ee4ce7993b476ee605
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-07 20:17:30 +0100

    change location for task array and functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import { fetchTasks, deleteTask, addTask } from "./api/tasks";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks().then(setTasks);
+    console.log(tasks);
+  }, []);
+
+  const DeleteTask = async (taskId) => {
+    await deleteTask(taskId);
+    const updatedTasks = await fetchTasks();
+    setTasks(updatedTasks);
+  };
+
+
   return (
     <div className="to-do-app-container bg-linear-to-tr from-blue-200 via-blue-400 to-blue-600 w-full h-screen flex justify-center items-center">
       <div className="to-do-app-main flex flex-col bg-linear-to-tr from-white/60 to-white/90 w-[80%] md:w-[90%] lg:w-[40%] h-[80%] sm:h-[70%] md:h-[60%] lg:h-[50%] p-[2em] rounded-xl shadow-lg">
         <Header />
         <TaskForm />
-        <TaskList />
+        <TaskList DeleteTask={DeleteTask} taskList={tasks} />
       </div>
     </div>
   );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
 import Task from "./Task";
-import { fetchTasks, deleteTask } from "../api/tasks";
 
-export default function TaskList() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    fetchTasks().then(setTasks);
-    console.log(tasks);
-  }, []);
-
-  const DeleteTask = async (taskId) => {
-    await deleteTask(taskId); 
-    const updatedTasks = await fetchTasks(); 
-    setTasks(updatedTasks);
-  };
-
+export default function TaskList({ DeleteTask, taskList }) {
   return (
     <div className="to-do-app-task-list flex flex-col space-y-4 overflow-y-auto flex-1">
-      {tasks.map((task) => (
+      {taskList.map((task) => (
         <Task
           key={task.id}
           taskName={task.taskName}
