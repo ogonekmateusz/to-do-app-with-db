e451fdb96aa3acea586fe72aa6af789d014282a4
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-07 19:02:45 +0100

    add header component with responsive styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="to-do-app-header">
+      <header>
+        <h1 className="font-bold bg-clip-text text-transparent bg-linear-to-tr from-blue-500 via-blue-600 to-blue-700 text-5xl md:text-6xl">
+          TO DO APP
+        </h1>
+      </header>
+    </div>
+  );
+}
