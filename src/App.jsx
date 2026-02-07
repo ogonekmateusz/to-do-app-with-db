9d29da66296334f5d843535de74b422602260e86
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-07 18:59:14 +0100

    fix responsive witdth in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="to-do-app-container bg-linear-to-tr from-blue-200 via-blue-400 to-blue-600 w-full h-screen flex justify-center items-center">
-      <div className="to-do-app-main bg-linear-to-tr from-white/60 to-white/90 w-[80%] md:w-[60%] lg:w-[35%] h-[80%] sm:h-[70%] md:h-[60%] lg:h-[50%] p-[2em] rounded-xl shadow-lg">
-        //todoform
+      <div className="to-do-app-main bg-linear-to-tr from-white/60 to-white/90 w-[80%] md:w-[0%] lg:w-[40%] h-[80%] sm:h-[70%] md:h-[60%] lg:h-[50%] p-[2em] rounded-xl shadow-lg">
+        <Header />
       </div>
     </div>
   );
