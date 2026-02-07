b2762f411928e4cb098e1bea03faa64f6dc15b50
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-07 20:51:16 +0100

    improve mobile responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
 
   return (
     <div className="to-do-app-container bg-linear-to-tr from-blue-200 via-blue-400 to-blue-600 w-full h-screen flex justify-center items-center">
-      <div className="to-do-app-main flex flex-col bg-linear-to-tr from-white/60 to-white/90 w-[80%] md:w-[90%] lg:w-[40%] h-[80%] sm:h-[70%] md:h-[60%] lg:h-[65%] p-[2em] rounded-xl shadow-lg">
+      <div className="to-do-app-main flex flex-col bg-linear-to-tr from-white/60 to-white/90 w-[80%] md:w-[90%] lg:w-[40%] h-[65%] sm:h-[70%] md:h-[60%] lg:h-[65%] p-[2em] rounded-xl shadow-lg">
         <Header />
         <TaskForm addTask={AddTask} />
         <TaskList DeleteTask={DeleteTask} taskList={tasks} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="to-do-app-header">
       <header>
-        <h1 className="font-bold bg-clip-text text-transparent bg-linear-to-tr from-blue-500 via-blue-600 to-blue-700 text-5xl md:text-6xl">
+        <h1 className="font-bold bg-clip-text text-transparent bg-linear-to-tr from-blue-500 via-blue-600 to-blue-700 text-4xl md:text-6xl">
           TO DO APP
         </h1>
       </header>
