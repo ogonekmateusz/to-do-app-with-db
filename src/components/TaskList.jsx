aac3fe6ac69186e2275579c9e3818bba438d5dcd
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-07 19:59:47 +0100

    add fetching tasks from db

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import Task from "./Task";
+import { fetchTasks } from "../api/tasks";
 
 export default function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks().then(setTasks);
+    console.log(tasks);
+  }, []);
+
   return (
     <div className="to-do-app-task-list flex flex-col space-y-4 overflow-y-auto flex-1">
-        <Task taskName={"example task"}/>
+      {tasks.map((task) => (
+        <Task key={task.id} taskName={task.taskName} />
+      ))}
     </div>
   );
 }
