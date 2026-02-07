7be09c60d874d37577ca0059dca8913e2e9466de
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-07 18:51:16 +0100

    add to do app default container with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="to-do-app-container bg-linear-to-tr from-blue-200 via-blue-400 to-blue-600 w-full h-screen flex justify-center items-center">
+      <div className="to-do-app-main bg-linear-to-tr from-white/60 to-white/90 w-[80%] md:w-[60%] lg:w-[35%] h-[80%] sm:h-[70%] md:h-[60%] lg:h-[50%]">
+        //todoform
+      </div>
+    </div>
+  );
 }
